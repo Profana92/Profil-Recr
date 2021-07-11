fbae73158f064d32cb614ec8b9e22f9d28760e62
Profana92 <87131514+Profana92@users.noreply.github.com>
2021-07-11 18:27:44 +0200

    Add files via upload

---
+++ js/countries.js
@@ -0,0 +1,166 @@
+//Deklaracja elementow po Id
+const countriesElement = document.getElementById('countries')
+const h2 = document.getElementById('countries')
+const searchEl = document.getElementById('search')
+getCountries();
+
+//Funkcja pobierajaca dane i wywolujaca displayCountries(countries)
+async function getCountries() 
+{
+	const res = await fetch('https://restcountries.eu/rest/v2/all');
+	const countries = await res.json();
+	displayCountries(countries);
+}
+
+// Funkcja wypisywania tabeli, iterujaca każdy rząd tabeli aż do jej zbudowania przy pomocy forEach, dodajaca jedna klase i ukrywająca tekst szukaj poprzed podmianke tekstu wewnatrz elementu na pusty
+function displayCountries(countries)
+{
+	countries.forEach(country => 
+  {
+	 const countryEl = document.createElement('tr')
+	 countryEl.classList.add('table-row')
+	 countryEl.innerHTML = `
+    <tr>
+    <td class=searchEle>${country.name}</td>
+    <td>${country.currencies.map(c =>`${c.name}`)}</td>
+    <td>${country.languages.map(c =>`${c.name}`)}</td>
+    <td class="aaa">${country.population}</td>
+    <td>${country.area}</td>
+    <td><img src="${country.flag}" alt="" /></td>
+    </tr>`;
+	 countriesElement.appendChild(countryEl);
+  });
+ const hiddenElement = document.getElementById('hidden')
+ hiddenElement.innerHTML = ``
+}
+
+//Funkcja sortująca tabelę wywoływania przez onclick w pliku HTML>
+function sortTable(n) 
+{
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById('countries');
+  switching = true;
+  dir = "asc";
+  while (switching) {
+      switching = false;
+      rows = table.getElementsByTagName("TR");
+      for (i = 1; i < (rows.length - 1); i++) 
+        {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+          var xContent = (isNaN(x.innerHTML)) ? (x.innerHTML.toLowerCase() === '-') ? 0 : x.innerHTML.toLowerCase() : parseFloat(x.innerHTML);
+          var yContent = (isNaN(y.innerHTML)) ? (y.innerHTML.toLowerCase() === '-') ? 0 : y.innerHTML.toLowerCase() : parseFloat(y.innerHTML);
+          if (dir == "asc") 
+            {
+             if (xContent > yContent) 
+              {
+               shouldSwitch= true;
+               break;
+              }
+            }
+            else if (dir == "desc") 
+            {
+             if (xContent < yContent) 
+             {
+              shouldSwitch= true;
+              break;
+             }
+            }
+        }
+      if (shouldSwitch) 
+       {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        switchcount ++;      
+        }
+        else 
+        {
+        if (switchcount == 0 && dir == "asc") 
+         {
+          dir = "desc";
+          switching = true;
+         }
+        }
+   }
+}
+
+//Funkcja przeszukująca tabęlę i ukrywająca rzędy nie istotne poprzez sprawdzenie indexOf komorek
+
+function search() 
+{
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("countries");
+  tr = table.getElementsByTagName("tr");
+  for (var i = 1; i < tr.length; i++) 
+  {
+    var tds = tr[i].getElementsByTagName("td");
+    var flag = false;
+    for(var j = 0; j < tds.length; j++)
+    {
+      var td = tds[j];
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) 
+        {
+          flag = true;
+        } 
+    }
+    if(flag)
+    {
+        tr[i].style.display = "table-row";
+    }
+    else 
+    {
+        tr[i].style.display = "none";
+    }
+  }
+}
+
+/*Funkcja przeszukująca tabęlę i ukrywa rzedy nie w zakresie, obie wartości nie musza
+ byc podane, jesli sa puste skrypt wstawia -1 (wystepuja kraje z zerową populacją) 
+ dla warosci minimalnej i maksymalną dla krajów samodzielnie na wywolaniu funkcji*/ 
+function searchminmax() 
+{
+  var input, filter, table, tr, td, i, min2, max2, minf, maxf;
+  minEl = document.getElementById("minpop");
+  maxEl = document.getElementById("maxpop");
+  min2 = minEl.value;
+  max2 = maxEl.value;   
+  if (max2 == "")
+  {
+    max2 =   13774221680;
+  if (min2 == "")
+  {
+    min2 = -1;
+  }
+  minf = parseFloat(min2);
+  maxf = parseFloat(max2);
+  table = document.getElementById("countries");
+  tr = table.getElementsByTagName("tr");
+  for (var i = 1; i < tr.length; i++) 
+    {
+      var tds = tr[i].getElementsByTagName("td");
+      var flag = false;
+      for(var j = 0; j < tds.length; j++)
+      {
+        var td = tds[j];
+        var tdparse = parseFloat(td.innerHTML)
+        if (tdparse > minf && tdparse < maxf ) 
+        {      
+          if(td.classList.contains("aaa"))
+          {
+            flag = true;
+          }
+        } 
+       }
+      if(flag)
+      {
+        tr[i].style.display = "table-row";
+      }
+      else 
+      {
+        tr[i].style.display = "none";
+      }
+    }
+}
---
+++ js/dashboard.js
@@ -0,0 +1,147 @@
+//Wywołani funkcji getCountries
+getCountries();
+//Potrzebuje potem dostepu do tej zmiennej spoza funkcji wiec deklaruje na poczatku dokumentu.
+let subarraylist = new Array();
+let subarraycurrencieslist = new Array();
+const countriesEl = document.getElementById('countries')
+
+async function getCountries()
+{
+  //Fetch Arraya
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const countries = await res.json();
+  //Przypisanie wartości początkowych
+  let popcount = 0;
+  let avgarea = 0;
+  let array = countries;
+
+  //Console Log do przeglądania całej tablicy ułatwia prace na danych przy pisaniu kodu
+  console.log(array);
+
+  /*Obliczanie ilości ludności poprzez ForEach przeszukujący tabelę i znajdujący wartości w kolejnych kolumnach i dodający je do poprzedniej wartości. 
+  Poprzez iteracje realizujemy od razu kroki dla średniej populacji i obszaru bo to ostatecznie to samo*/
+  array.forEach(country => 
+  {
+    popcount = popcount + country.population
+    avgarea = avgarea + country.area
+  });
+
+  // utworzenie subarraya z samymi językami i wpisanie ich do nowej tablicy nazwanej subarray
+  let subarray = new Array();
+  let i = 0;
+  array.forEach(country => 
+  {
+    subarray[i] = country.languages.map(c =>`${c.name}`);
+    i=i+1;
+    return subarray;
+  });
+
+  // utworzenie subarraya z samymi walutami i wpisanie ich do nowej tablicy nazwanej subarraycurrencies
+  let subarraycurrencies = new Array();
+  i = 0;
+  array.forEach(country => 
+  {
+    subarraycurrencies[i] = country.currencies.map(c =>`${c.name}`);
+    i=i+1;
+    return subarraycurrencies;
+  });
+
+  // utworzenie subarraya z samymi sasiadami i wpisanie ich do nowej tablicy nazwanej subarrayborders
+  let subarrayborders = new Array();
+  i = 0;
+  array.forEach(country => 
+  {
+    subarrayborders[i] = country.borders.map(c =>`${c.name}`);
+    i=i+1;
+    return subarrayborders;
+  });
+
+  let countriescount = 0
+  //Przeszukanie subarrayborders
+  for ( i=0; i<subarrayborders.length; i++ )
+  {
+    countriescount = countriescount + subarrayborders[i].length;
+  }
+
+  //Utworzenie jednowymiarowego arraya poprzez przeszukanie całej subarray i utworzenie nowej o uproszczonej strukturze subarraylist
+  for ( i=0; i<subarray.length; i++ )
+  {
+    for ( j=0; j<subarray[i].length; j++ )
+    {
+      subarraylist.push(subarray[i][j]);
+    }
+  }
+
+    //Utworzenie jednowymiarowego arraya poprzez przeszukanie całej subarraycurrencies i utworzenie nowej o uproszczonej strukturze subarraycurrencieslist
+  for ( i=0; i<subarraycurrencies.length; i++ )
+  {
+    for ( j=0; j<subarraycurrencies[i].length; j++ )
+    {
+      subarraycurrencieslist.push(subarraycurrencies[i][j]);
+    }
+  }
+
+  /* Konwersja tablicy jednowymiarowej na string i użycie splitu w celu rozdzialu stringa przez przecinek, potem określenie najbardziej popularnych języków
+  globalnie a nie względem kraju, zwraca wartość języków używanych w największej ilości krajów a nie te, którymi posługuje się najwieksza liczba ludności*/
+  let str = subarraylist.toString();
+  const num = 5;
+  const findMostFrequent = (subarraylist, num) => 
+  {
+   const strArr = str.split(',');
+   const map = {};
+   strArr.forEach(word =>
+    {
+      if(map.hasOwnProperty(word))
+      {
+         map[word]++;
+      }
+      else
+      {
+         map[word] = 1;
+      }
+    });
+   const frequencyArr = Object.keys(map).map(key => [key, map[key]]);
+   frequencyArr.sort((a, b) => b[1] - a[1]);
+   return frequencyArr.slice(0, num).map(el => el[0]);
+
+  };
+  //To samo dla waluty
+  let str2 = subarraycurrencieslist.toString();
+  const num2 = 5;
+  const findMostFrequentCurrency = (subarraycurrencieslist, num2) => {
+  const strArr2 = str2.split(',');
+  const map2 = {};
+  strArr2.forEach(word =>
+  {
+    if(map2.hasOwnProperty(word))
+    {
+      map2[word]++;
+    }
+    else
+    {
+      map2[word] = 1;
+    }
+  });
+   const frequencyArr = Object.keys(map2).map(key => [key, map2[key]]);
+   frequencyArr.sort((a, b) => b[1] - a[1]);
+   return frequencyArr.slice(0, num).map(el => el[0]);
+  };
+
+  /* Wypisanie w oknie przeglądarki wraz z dzieleniem przez array.lenght (równie dobrze zadziała liczba 250, ale w przypadku zmian w arrayu jak np. usuniecie kraju
+  zniszczy wynik obliczeń, bardziej sensownym jest więc użycie array.lenght)*/
+  countriesEl.innerHTML = 
+  `<div class="countriesEl"
+     Number of countries on earth: ${subarray.length}</br>
+     Number of countries on earth: ${array.length}</br>
+     5 Most common languages are: ${findMostFrequent(str, num)}</br>          
+     5 Most common Currencies are: ${findMostFrequentCurrency(str2, num2)}</br>
+     Average number of neigbours: ${countriescount/array.length}</br>
+     Average number of people in a country: ${popcount/array.length}</br>
+     Average area for a country: ${avgarea/array.length} quare km</br>
+  </div>`
+
+  const hiddenElement = document.getElementById('hidden')
+  hiddenElement.innerHTML = ``
+}
+
+
